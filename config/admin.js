8d2eea1a752b8178e30fa126eeafbd928b5ba2c9
AbhinavPWN <abhinavkarki05@gmail.com>
2025-08-26 19:24:03 +0000

    Initial commit for dev branch

---
+++ config/admin.js
@@ -0,0 +1,21 @@
+module.exports = ({ env }) => ({
+  url: '/keystone-admin-2082',
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
+  contentTypeBuilder: {
+    enabled: true, // Enable Content-Type Builder
+  },
+  flags: {
+    nps: env.bool('FLAG_NPS', true),
+    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+  },
+});
